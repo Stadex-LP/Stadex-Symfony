5b17be356b7c57b1076ed95a69b4d7fd7e1a1f06
Niruchan Arulanand Arajah <niruchan.arulanand-arajah@etu.u-paris.fr>
2023-03-16 16:35:14 +0100

    add Organisateur model and API resource

--- src/Entity/Manifestation.php
+++ src/Entity/Manifestation.php
@@ -3,30 +3,46 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\ManifestationRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ManifestationRepository::class)]
 #[ApiResource]
+#[Get(normalizationContext: ['groups' => ['manisfestation:read']])]
+#[GetCollection(normalizationContext: ['groups' => ['manisfestations:read']])]
 class Manifestation
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['manisfestation:read', 'manisfestations:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['manisfestation:read', 'manisfestations:read'])]
     private ?string $denomination = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['manisfestation:read', 'manisfestations:read'])]
     private ?\DateTimeInterface $dateDebut = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['manisfestation:read', 'manisfestations:read'])]
     private ?\DateTimeInterface $dateFin = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['manisfestation:read', 'manisfestations:read'])]
     private ?string $lieu = null;
+
+    #[ORM\ManyToOne(inversedBy: 'manifestations')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups('manisfestation:read')]
+    private ?Organisateur $organisateur = null;
 
     public function getId(): ?int
     {
@@ -77,6 +93,18 @@ class Manifestation
     public function setLieu(string $lieu): self
     {
         $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getOrganisateur(): ?Organisateur
+    {
+        return $this->organisateur;
+    }
+
+    public function setOrganisateur(?Organisateur $organisateur): self
+    {
+        $this->organisateur = $organisateur;
 
         return $this;
     }
---
+++ src/Entity/Organisateur.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use App\Repository\OrganisateurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: OrganisateurRepository::class)]
+#[ApiResource]
+#[Get(normalizationContext: ['groups' => ['organisateur:read']])]
+#[GetCollection(normalizationContext: ['groups' => ['organisateurs:read']])]
+class Organisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['organisateur:read','organisateurs:read','manisfestation:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['organisateur:read','organisateurs:read','manisfestation:read'])]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['organisateur:read','organisateurs:read','manisfestation:read'])]
+    private ?string $serviceDemandeur = null;
+
+    #[ORM\OneToMany(mappedBy: 'organisateur', targetEntity: Manifestation::class)]
+    private Collection $manifestations;
+
+    public function __construct()
+    {
+        $this->manifestations = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getServiceDemandeur(): ?string
+    {
+        return $this->serviceDemandeur;
+    }
+
+    public function setServiceDemandeur(string $serviceDemandeur): self
+    {
+        $this->serviceDemandeur = $serviceDemandeur;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Manifestation>
+     */
+    public function getManifestations(): Collection
+    {
+        return $this->manifestations;
+    }
+
+    public function addManifestation(Manifestation $manifestation): self
+    {
+        if (!$this->manifestations->contains($manifestation)) {
+            $this->manifestations->add($manifestation);
+            $manifestation->setOrganisateur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeManifestation(Manifestation $manifestation): self
+    {
+        if ($this->manifestations->removeElement($manifestation)) {
+            // set the owning side to null (unless already changed)
+            if ($manifestation->getOrganisateur() === $this) {
+                $manifestation->setOrganisateur(null);
+            }
+        }
+
+        return $this;
+    }
+}
