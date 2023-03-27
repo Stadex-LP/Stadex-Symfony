bcad5c2b06c95e2f88d60af5fbce11440756161e
Axel-1 <65816310+Axel-1@users.noreply.github.com>
2023-03-27 16:06:31 +0200

    Fix database create in production environment

--- .docker/php-prod/docker-entrypoint.sh
+++ .docker/php-prod/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-bin/console doctrine:database:create --no-interaction
+bin/console doctrine:database:create --no-interaction --if-not-exists
 
 bin/console doctrine:migrations:migrate --no-interaction
 
