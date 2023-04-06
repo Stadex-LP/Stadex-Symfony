f8253d8b4403878d91f7fddce67afcc125987233
Axel-1 <65816310+Axel-1@users.noreply.github.com>
2023-04-06 15:23:15 +0200

    Add Manifestation CRUD

---
+++ src/Controller/ManifestationController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Manifestation;
+use App\Form\ManifestationType;
+use App\Repository\ManifestationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/manifestation')]
+class ManifestationController extends AbstractController
+{
+    #[Route('/', name: 'app_manifestation_index', methods: ['GET'])]
+    public function index(ManifestationRepository $manifestationRepository): Response
+    {
+        return $this->render('manifestation/index.html.twig', [
+            'manifestations' => $manifestationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_manifestation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ManifestationRepository $manifestationRepository): Response
+    {
+        $manifestation = new Manifestation();
+        $form = $this->createForm(ManifestationType::class, $manifestation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manifestationRepository->save($manifestation, true);
+
+            return $this->redirectToRoute('app_manifestation_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('manifestation/new.html.twig', [
+            'manifestation' => $manifestation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_manifestation_show', methods: ['GET'])]
+    public function show(Manifestation $manifestation): Response
+    {
+        return $this->render('manifestation/show.html.twig', [
+            'manifestation' => $manifestation,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_manifestation_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Manifestation $manifestation, ManifestationRepository $manifestationRepository): Response
+    {
+        $form = $this->createForm(ManifestationType::class, $manifestation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manifestationRepository->save($manifestation, true);
+
+            return $this->redirectToRoute('app_manifestation_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('manifestation/edit.html.twig', [
+            'manifestation' => $manifestation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_manifestation_delete', methods: ['POST'])]
+    public function delete(Request $request, Manifestation $manifestation, ManifestationRepository $manifestationRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$manifestation->getId(), $request->request->get('_token'))) {
+            $manifestationRepository->remove($manifestation, true);
+        }
+
+        return $this->redirectToRoute('app_manifestation_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ManifestationType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Manifestation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ManifestationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('denomination')
+            ->add('dateDebut', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('dateFin', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('lieu')
+            ->add('organisateur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Manifestation::class,
+        ]);
+    }
+}
